ca499be52ef71414dae3e4268950cead7530e831
mdshakib107 <mdshakib105100@gmail.com>
2021-10-30 21:35:12 +0600

    heroku test

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 
 const app = express()
 const port = process.env.PORT || 5000;
+
 //Midelewere
 app.use(cors());
 app.use(express.json());
@@ -18,7 +19,7 @@ async function run() {
         await client.connect();
         const database = client.db('allPackages');
         const packageCollection = database.collection('packages');
-        console.log('ok its working')
+        const ordersCollection = database.collection("orders");
         // GET API
         app.get('/package', async (req, res) => {
             const cursor = packageCollection.find({});
@@ -27,31 +28,46 @@ async function run() {
         });
 
         // GET Single Service
-        // app.get('/package/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     console.log('getting specific package', id);
-        //     const query = { _id: ObjectId(id) };
-        //     const package = await packageCollection.findOne(query);
-        //     res.json(package);
-        // })
+        app.get('/singlePackage/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const package = await packageCollection.findOne(query);
+            res.json(package);
+        })
 
-        // POST API
+        // POST PACKAGE API
         app.post('/package', async (req, res) => {
             const package = req.body;
-            console.log('hit the post api', package);
-
             const result = await packageCollection.insertOne(package);
-            console.log(result);
             res.json(result)
         });
+        // POST PLACE ORDER API
+        app.post('/placeorder', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.json(result)
+        });
+        // GET PLACE ORDER API
+        app.get("/myOrders/:email", async (req, res) => {
+
+            const email = req.params.email;
+            const query = { userEmail: email };
+            const package = await ordersCollection.find(query).toArray();
+            res.json(package);
+        });
+
+
 
         // DELETE API
-        // app.delete('/package/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const result = await packageCollection.deleteOne(query);
-        //     res.json(result);
-        // })
+        app.delete('/deleteProduct/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id)
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            console.log(result)
+            res.json(result);
+
+        })
 
     }
     finally {
@@ -59,11 +75,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
-app.get('/', (req, res) => {
-    res.send('Running Genius Server');
-});
-
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
